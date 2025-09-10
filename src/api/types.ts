760ccd5ed3bb39b749b1aa4300e22b5788670ee9
KaterynaNat <kate.naturkach@gmail.com>
2025-09-10 17:19:53 +0300

    Redux prep., libraries inst.

---
+++ src/api/types.ts
@@ -0,0 +1,15 @@
+export type MovieSummary = {
+  id: number;
+  title: string;
+  original_title: string;
+  poster_path: string | null;
+  release_date?: string;
+  genre_ids?: number[];
+};
+
+export type PaginatedResult<T> = {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+};
