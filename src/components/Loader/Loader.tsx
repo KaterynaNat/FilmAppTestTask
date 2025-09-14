0b0d7a04810390a65529c5c22d929484bee23224
KaterynaNat <kate.naturkach@gmail.com>
2025-09-14 18:04:20 +0300

    Cange loader, fix footer

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,10 @@
+import { PacmanLoader } from "react-spinners";
 import s from "./Loader.module.css";
 
 const Loader = () => (
-  <div className={s.wrap}>
-    <div className={s.spinner} />
-    <p>🎬 Fetching the popcorn…</p>
+  <div className={s.wrap} role="status" aria-label="Loading">
+    <PacmanLoader color="#7948b4ff" size={28} speedMultiplier={1} />
+    <p>🍿 Fetching the popcorn…</p>
   </div>
 );
 
