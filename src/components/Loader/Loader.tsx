0725a87cbe3d120bd655f5900216086746cb9981
KaterynaNat <kate.naturkach@gmail.com>
2025-09-14 17:39:14 +0300

    Add spinner loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,3 +1,10 @@
 import s from "./Loader.module.css";
-const Loader = () => <div className={s.wrap}>Loading…</div>;
+
+const Loader = () => (
+  <div className={s.wrap}>
+    <div className={s.spinner} />
+    <p>🎬 Fetching the popcorn…</p>
+  </div>
+);
+
 export default Loader;
